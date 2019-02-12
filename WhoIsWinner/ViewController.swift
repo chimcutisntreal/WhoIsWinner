87545f6f9cd0ab96317f3bd19b3bc4706b1373e3
Chinh <chinh@chinhs-macbook-pro.local>
2019-02-12 15:02:51 +0700

    Initial Commit

---
+++ WhoIsWinner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhoIsWinner
+//
+//  Created by Chinh on 2/12/19.
+//  Copyright © 2019 Chinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
